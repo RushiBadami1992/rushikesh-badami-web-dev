1b0c71075c994f887bc340b9d88903276beb0b68
rushibadami1992 <rushibadami1992@gmail.com>
2016-06-17 15:07:02 -0400

    publishing to OpenShift

--- public/assignment/app.js
+++ public/assignment/app.js
@@ -1,5 +1,6 @@
 // IIFE (immediately invoked function expression
 'use strict';
+
 (function(){
     angular.module("WebAppMaker", ["ngRoute","textAngular","wamDirectives"]);
 })();
--- server.js
+++ server.js
@@ -9,7 +9,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 
 //require ("./test/app.js")(app);
+var connectionString = 'mongodb://127.0.0.1:27017/test';
 
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+        process.env.OPENSHIFT_APP_NAME;
+}
+var mongoose = require('mongoose');
+mongoose.connect(connectionString);
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
 var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
